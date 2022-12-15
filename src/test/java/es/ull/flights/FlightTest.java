34dbbfb26b6c6df630f8f790a5217596bf6d9071
alu0101330600 <65622828+jordidogo@users.noreply.github.com>
2022-12-15 09:57:06 +0000

    Creados nuevos test

--- src/test/java/es/ull/flights/FlightTest.java
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -11,13 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FlightTest {
 
     private Flight flight;
+    private Flight flight2;
     private Passenger passenger;
+    private Passenger passenger2;
     private final String flightNumber = "CD8230";
+    private final String flightNumber2 = "CD8330";
 
     @BeforeEach
     void setUp() {
         flight = new Flight(flightNumber, 1);
+        flight2 = new Flight(flightNumber2, 30);
         passenger = new Passenger("1", "Jorge", "ES");
+        passenger2 = new Passenger("2", "Ruyman", "ES");
     }
 
     @Test
@@ -26,7 +31,10 @@ public class FlightTest {
         assertAll("Verify all attributes are set correctly",
                 () -> assertEquals(flightNumber, flight.getFlightNumber(), "Flight Number"),
                 () -> assertTrue(flight.addPassenger(passenger), "Adding a passenger"),
-                () -> assertEquals(1, flight.getNumberOfPassengers(), "Number of passengers")
+                () -> assertEquals(1, flight.getNumberOfPassengers(), "Number of passengers"),
+                () -> assertEquals(flightNumber2, flight2.getFlightNumber(), "Flight Number"),
+                () -> assertTrue(flight2.addPassenger(passenger2), "Adding a passenger"),
+                () -> assertEquals(1, flight2.getNumberOfPassengers(), "Number of passengers")
         );
     }
 
@@ -47,6 +55,17 @@ public class FlightTest {
                 () -> assertTrue(flight.addPassenger(passenger), "Adding a passenger"),
                 () -> assertTrue(flight.removePassenger(passenger), "We can remove him"),
                 () -> assertEquals(0, flight.getNumberOfPassengers(), "No passengers on the flight")
+        );
+    }
+    
+    @Test
+    @DisplayName("We can verify flight information and flight passengers")
+    public void testAttributesFlight() {
+        assertAll("Verify conditions for a flight",
+                () -> assertEquals("CD8230", flight.getFlightNumber()),
+                () -> assertEquals("CD8330", flight2.getFlightNumber()),
+                () -> assertEquals(0, flight.getNumberOfPassengers()),
+                () -> assertEquals(0, flight2.getNumberOfPassengers())
         );
     }
 }
--- src/test/java/es/ull/passengers/PassengerTest.java
+++ src/test/java/es/ull/passengers/PassengerTest.java
@@ -11,21 +11,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PassengerTest {
 
     private Passenger passenger;
+    private Passenger passenger2;
 
     @BeforeEach
     void setUp() {
-        String countryCode = "ES";
-        passenger = new Passenger("1", "Jorge", countryCode);
+        passenger = new Passenger("1", "Jorge", "ES");
+        passenger2 = new Passenger("2", "Ruyman", "ES");
     }
 
     @Test
-    @DisplayName("Class Getters")
+    @DisplayName("Verify Information")
     void testFlightGetters() {
         assertAll("Verify all attributes are set correctly",
                 () -> assertEquals("1", passenger.getIdentifier(), "Passenger ID"),
                 () -> assertEquals("Jorge", passenger.getName(), "Passenger Name"),
                 () -> assertEquals("ES", passenger.getCountryCode(), "Passenger Country Code"),
-                () -> assertEquals("Passenger Jorge with identifier: 1 from ES" , passenger.toString())
+                () -> assertEquals("Passenger Jorge with identifier: 1 from ES" , passenger.toString()),
+                
+                () -> assertEquals("2", passenger2.getIdentifier(), "Passenger ID"),
+                () -> assertEquals("Ruyman", passenger2.getName(), "Passenger Name"),
+                () -> assertEquals("ES", passenger2.getCountryCode(), "Passenger Country Code"),
+                () -> assertEquals("Passenger Ruyman with identifier: 2 from ES" , passenger2.toString())
         );
     }
 
@@ -37,7 +43,10 @@ public class PassengerTest {
         assertAll("Verify we can join a flight",
                 () -> assertDoesNotThrow(()-> passenger.joinFlight(flight), "We can join a flight"),
                 () -> assertEquals(flight, passenger.getFlight(), "The flight is set correctly"),
-                () -> assertThrows(RuntimeException.class, () -> passenger.joinFlight(emptyFlight), "We cannot join a flight")
+                () -> assertThrows(RuntimeException.class, () -> passenger.joinFlight(emptyFlight), "We cannot join a flight"),
+                () -> assertDoesNotThrow(()-> passenger2.joinFlight(flight), "We can join a flight"),
+                () -> assertEquals(flight, passenger2.getFlight(), "The flight is set correctly"),
+                () -> assertThrows(RuntimeException.class, () -> passenger2.joinFlight(emptyFlight), "We cannot join a flight")
         );
     }
 }
